2ef4f19dc724d2b99d262850b67636490a96240b
Saugat Rai <saugatrai33@gmail.com>
2019-12-04 06:56:32 +0545

    render model to scene.

---
+++ app/src/main/java/com/example/ardemo2/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.ardemo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.MaterialFactory;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.arFragment);
+
+        initTapListener();
+
+//        runOnEmulator();
+    }
+
+    private void runOnEmulator() {
+        arFragment.setOnTapArPlaneListener(((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+            MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
+                    .thenAccept(material -> {
+                        ModelRenderable renderable = ShapeFactory.makeSphere(1.0f,
+                                new Vector3(0f, 1f, 1f), material);
+                        AnchorNode anchorNode = new AnchorNode(anchor);
+                        anchorNode.setRenderable(renderable);
+                        arFragment.getArSceneView().getScene().addChild(anchorNode);
+                    });
+        }));
+    }
+
+    private void initTapListener() {
+        arFragment.setOnTapArPlaneListener(((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("ArcticFox_posed.sfb"))
+                    .build()
+                    .thenAccept(modelRenderable ->
+                            addModelToScene(anchor, modelRenderable))
+                    .exceptionally(throwable -> {
+                        Toast.makeText(this,
+                                "Error rendering model", Toast.LENGTH_SHORT).show();
+                        return null;
+                    });
+        }));
+    }
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode transformableNode = new TransformableNode(
+                arFragment.getTransformationSystem());
+        transformableNode.setParent(anchorNode);
+        transformableNode.setRenderable(modelRenderable);
+        arFragment.getArSceneView().getScene()
+                .addChild(anchorNode);
+        transformableNode.select();
+    }
+}
